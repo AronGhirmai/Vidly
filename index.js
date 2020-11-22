5165f4626aaa0917f83325ac3f72ce3c7b504559
AronGhirmai <aron.ghirmai@gmail.com>
2020-11-22 13:49:35 +0300

    Added initial packages

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-testing
+const winston = require('winston');
+const express = require('express');
+const app = express();
+
+// Error Logging 
+require('./startup/logging')();
+// run all middleware
+require('./startup/middleware')(app);
+// run all routes Middleware
+require('./startup/routes')(app); //will implement all the routes
+// Connect to mongodb 
+require('./startup/db')();
+// check environment
+require('./startup/config')();
+// Joi validation
+require('./startup/validation')();
+// production middleware
+require('./startup/prod')(app);
+
+const PORT = process.env.PORT || 3000;
+const server = app.listen(PORT, () => {
+    winston.info(`Listening to port :${PORT}`);
+});
+
+module.exports = server;
